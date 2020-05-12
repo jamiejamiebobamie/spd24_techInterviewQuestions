c985225e1fdb857aab63e85c6eb742e676ed179e
jamiejamiebobamie <jmccrory@vt.edu>
2020-05-12 15:22:41 -0700

    corrected my solution and added edge cases

--- meeting_rooms.py
+++ meeting_rooms.py
@@ -7,48 +7,64 @@ sample
 
 """
 
-
 def min_rooms(meeting_times):
     # put all of the start and end times in a dictionary
     # with start times as the keys
-    # and ending times as the value.
-    time_lookup = {}
+    # and ending times as values (as an array in case of duplicate start times).
+    starting_times = {}
 
-    # add the times to the 'time_lookup' dict as well as find when the last meeting is over.
+    # add the times to the 'starting_times' dict as well as find when the last meeting is over.
     last_meeting_time_end = 0
     for start,end in meeting_times:
         last_meeting_time_end = max(last_meeting_time_end, end)
-        time_lookup[start] = end
+        # in case of meetings with duplicate start times,
+        # create a start time key with array of end times as the values.
+        if start not in starting_times:
+            starting_times[start] = [end]
+        else:
+            starting_times[start].append(end)
 
-    ending_times = []
+
+    ending_times = {}
     most_rooms = 0
-    # iterate through all of the minutes from now until the conclusion of the last meetin.
+    # iterate through all of the minutes from now until the conclusion of the last meeting.
     for minute in range(last_meeting_time_end+1):
-        # compute the max number of rooms given the current state
-        # of the ending_times array which signifies the total #
-        # of meeting in progress currently.
-        most_rooms = max(len(ending_times),most_rooms)
-        # peek at the top of the ending time stack
-        # (this is the problem with my solution and why it will not work,
-        # and if it does work ... why???)
+        # compute the sum of all the ending_times.values()
+        # (the sum of all of the array lengths)
+        # as this signifies the total #
+        # of meetings in progress.
+        sum = 0
+        for values in ending_times.values():
+            sum+=len(values)
+        most_rooms = max(sum,most_rooms)
+
+        # if the dict is not empty (if there are meetings in progress)
         if len(ending_times) != 0:
-            last_index_of_array = len(ending_times)-1
-            top_of_ending_times_stack = ending_times[last_index_of_array]
-            # if the meeting's ending time is the current minute,
-            # pop it from the meeting ending_times array
-            if top_of_ending_times_stack == minute:
-                ending_times.pop()
+            # if the current minute is an end_time for a meeting.
+            # remove it from the dictionary, regardless of which start_time
+            # (if several) started the meeting, all meetings with that end time
+            # are now over.
+            if minute in ending_times:
+                del ending_times[minute]
         # finally check if the current minute is in the meeting times
         # 'time_lookup' dictionary and if it is at the endingtime to the
-        # ending_times array to beign the meeting.
-        if minute in time_lookup:
-            meeting_end = time_lookup[minute]
-            ending_times.append(meeting_end)
+        # ending_times array to begin the meeting.
+        if minute in starting_times:
+            meeting_end_times_for_that_start_time = starting_times[minute]
+            for end_time in meeting_end_times_for_that_start_time:
+                if end_time not in ending_times:
+                    ending_times[end_time] = [minute]
+                else:
+                    ending_times[end_time].append(minute)
 
     return most_rooms
 
 
-arr = [[0,30],[5,10],[15,20]]
-arr = [[7,10],[2,4]]
+# arr = [[0,30],[5,10],[15,20]]
+# arr = [[7,10],[2,4]]
+arr = [[3,10],[5,6],[4,6]]
+
+# O(n+l) runtime n being the number of meeting times
+# and l being the range of 0 to the last meeting's
+# end time. O(n) storage.
 print(min_rooms(arr))
-# it works...
