f1810f897b0762fd816e1f35adcc440cb3ced90f
jamiejamiebobamie <jmccrory@vt.edu>
2020-05-14 22:09:03 -0700

    removed excess code and edited comments.

--- asteroid_collision.py
+++ asteroid_collision.py
@@ -47,9 +47,8 @@ Each asteroid will be a non-zero integer in the range [-1000, 1000]..
 
 """
 
-# this works.
 # O(n) time complexity.
-# O(1) space.
+# O(n) space.
 def collision(asteroid_array):
     # an array of asteroids that are on the left and moving right
     colliders = []
@@ -57,50 +56,48 @@ def collision(asteroid_array):
     for i, asteroid in enumerate(asteroid_array):
         # a potential asteroid from colliders array
         collider = None
-        # if the current element is not None
-        if asteroid:
-            # if the current asteroid is moving right
-                # add it to the colliders array
-            if asteroid > 0:
-                colliders.append(asteroid)
-                collider_index_array.append(i)
-            # else if it's moving left
-                # check if there are any asteroids moving right.
-                # and if there are, pop the the closest one.
+        # if the current asteroid is moving right
+            # add it to the colliders array
+        if asteroid > 0:
+            colliders.append(asteroid)
+            collider_index_array.append(i)
+        # else if it's moving left
+            # check if there are any asteroids in the collider array
+            # (moving right) and if there are, pop the the closest one.
+        else:
+            if len(colliders):
+                collider = colliders.pop()
+                collider_index = collider_index_array.pop()
+        # if we have a collision (if collider is not None)
+            # check the current asteroid against the collider
+        while collider and asteroid:
+            # if they're of equal size. set everything to None.
+            if abs(collider) == abs(asteroid):
+                asteroid_array[collider_index] = None
+                asteroid_array[i] = None
+                collider = None
+                asteroid = None
+            # if the right side asteroid (the collider) is larger.
+                # set the (left side) asteroid to None
+            elif abs(collider) > abs(asteroid):
+                asteroid_array[i] = None
+                asteroid = None
+            # if the (left side) asteroid is larger.
+                # set the collider to None
+                # and grab the next one from the stack.
             else:
-                if len(colliders):
+                asteroid_array[collider_index] = None
+                collider = None
+                if colliders:
                     collider = colliders.pop()
                     collider_index = collider_index_array.pop()
-            # if we have a collision (if collider is not None
-                # check if the current asteroid against the collider
-            while collider and asteroid:
-                # if they're of equal size. set everything to None
-                if abs(collider) == abs(asteroid):
-                    asteroid_array[collider_index] = None
-                    asteroid_array[i] = None
-                    collider = None
-                    asteroid = None
-                # if the right side asteroid is larger.
-                    # set the (left) asteroid to None
-                elif abs(collider) > abs(asteroid):
-                    asteroid_array[i] = None
-                    asteroid = None
-                # if the left side asteroid is larger.
-                    # set the collider to None
-                    # and grab the next one from the stack.
-                else:
-                    asteroid_array[collider_index] = None
-                    collider = None
-                    if colliders:
-                        collider = colliders.pop()
-                        collider_index = collider_index_array.pop()
 
     return [asteroid for asteroid in asteroid_array if asteroid != None]
 
 
 asteroids = [-2, -1, 1, 2] # [-2, -1, 1, 2]
 asteroids = [8, -8] # []
-asteroids = [5, 10, -5] # [5, 10]
+# asteroids = [5, 10, -5] # [5, 10]
 # asteroids = [10, 2, -5] # [10]
 
 print(collision(asteroids))
